5935c89c66eb9f40d2f12ca998b04e7ea3e82e5a
AymenXD <41294956+AymenXD@users.noreply.github.com>
2018-07-17 14:11:39 +0100

    Add files via upload

---
+++ js/cloning.js
@@ -0,0 +1,121 @@
+$(document).ready(function(){
+    $("#first").hover(function(){
+        $("#myDIV1").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $("#second").hover(function(){
+        $("#myDIV2").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $("#third").hover(function(){
+        $("#myDIV3").slideToggle("slow");
+    });
+});
+
+
+$(document).ready(function(){
+    $("#design").hover(function(){
+        $("#design").css("transform","translate(200px,0)");
+        $("#design").css("opacity", 1);
+
+    });
+});
+$(document).ready(function(){
+    $("#development").hover(function(){
+        $("#development").css("transform","translate(200px,0)");
+        $("#development").css("opacity", 1);
+
+    });
+});
+$(document).ready(function(){
+    $("#marketing").hover(function(){
+        $("#marketing").css("transform","translate(200px,0)");
+        $("#marketing").css("opacity", 1);
+
+    });
+});
+
+
+
+
+$(document).ready(function(){
+    $("#bouton").click(function(){
+        $("#rf").hide("slow");
+        $("#foo").css('marginTop',"70em");
+    });
+});
+
+
+
+
+$(document).ready(function(){
+    $('#aa').click(function(){
+        var src=document.getElementById("aa").src
+        document.getElementById("pf").src=src
+        document.getElementById("galerie").style.visibility="visible"
+    })
+})
+
+$(document).ready(function(){
+    $('#ab').click(function(){
+        var src=document.getElementById("ab").src
+        document.getElementById("pf").src=src
+        document.getElementById("galerie").style.visibility="visible"
+    })
+})
+
+$(document).ready(function(){
+    $('#ac').click(function(){
+        var src=document.getElementById("ac").src
+        document.getElementById("pf").src=src
+        document.getElementById("galerie").style.visibility="visible"
+    })
+})
+
+
+$(document).ready(function(){
+    $('#ad').click(function(){
+        var src=document.getElementById("ad").src
+        document.getElementById("pf").src=src
+        document.getElementById("galerie").style.visibility="visible"
+    })
+})
+
+$(document).ready(function(){
+    $('#ae').click(function(){
+        var src=document.getElementById("ae").src
+        document.getElementById("pf").src=src
+        document.getElementById("galerie").style.visibility="visible"
+    })
+})
+
+$(document).ready(function(){
+    $('#af').click(function(){
+        var src=document.getElementById("af").src
+        document.getElementById("pf").src=src
+        document.getElementById("galerie").style.visibility="visible"
+    })
+})
+
+$(document).ready(function(){
+    $('#ag').click(function(){
+        var src=document.getElementById("ag").src
+        document.getElementById("pf").src=src
+        document.getElementById("galerie").style.visibility="visible"
+    })
+})
+
+$(document).ready(function(){
+    $('#ah').click(function(){
+        var src=document.getElementById("ah").src
+        document.getElementById("pf").src=src
+        document.getElementById("galerie").style.visibility="visible"
+    })
+})
+
+
+
+
+
